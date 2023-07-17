25d904c527041583575df5b01e197bcd99c156fc
Sergio Pineda <78764356+SergioAPineda@users.noreply.github.com>
2023-07-16 20:35:00 -0400

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -9,8 +9,6 @@ router.put('/edit/:id', usersController.updateuser);
 
 router.delete('/delete/:id', usersController.deleteUser);
 
-module.exports = router;
-
 router.get('/:id', usersController.getUserById); 
 
 module.exports = router;
