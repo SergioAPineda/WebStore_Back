278d3955f3642a36ac27eac0aadfdac125abab5c
Sergio Pineda <78764356+SergioAPineda@users.noreply.github.com>
2023-07-18 13:57:02 -0400

    Delete Product working

--- routes/product.js
+++ routes/product.js
@@ -4,5 +4,8 @@ let productController = require('../controllers/product');
 
 router.post('/new', productController.createProduct);
 router.put('/update/:id', productController.updateProduct);
+router.get('/', productController.getProductList);
+router.get('/:id', productController.getProduct);
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
