3b5b71f47dc99218ba017ba83a600ea723e135f4
Muhammad Abdulsalam <75138704+cyberrebelx@users.noreply.github.com>
2023-08-11 17:12:11 -0400

    Backend update

--- routes/question.js
+++ routes/question.js
@@ -6,7 +6,8 @@ let authController = require('../controllers/auth');
 router.post('/new', questionController.createQuestion);
 router.put('/update/:id', authController.requireAuth, authController.isAllowedToAnswer, questionController.updateQuestion);
 router.get('/product/:id', questionController.getQuestionList);
-router.get('/:id', questionController.getQuestion);
+router.get('questions/:id', questionController.getQuestion);
+router.get('questions/:id/answers', questionController.getQuestion);
 router.delete('/:id', authController.requireAuth, authController.isAllowedToAnswer, questionController.deleteQuestion);
 
 module.exports = router;
